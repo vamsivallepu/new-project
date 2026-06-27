ca9ab56472efd69da0a243416126fb78f65f6c84
AVB934 <avbolagoni@gmail.com>
2026-06-27 12:39:06 +0100

    made file called model

---
+++ model.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleNN(nn.Module):
+    def __init__(self,input_size):
+        super().__init__()
+        self.fc1 = nn.Linear(input_size,64)
+        self.fc2 = nn.Linear(64,32)
+        self.fc3 = nn.Linear(32,1)
+    
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = torch.Sigmoid(self.fc3(x))
+    
+        return x
